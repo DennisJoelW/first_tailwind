8809a01669cedfef31fdfc622426455cd5fcffc5
TryanJaya <tryanjaya33.tj@gmail.com>
2024-04-29 11:20:07 +0700

    Crypto card

--- src/components/Wallet.jsx
+++ src/components/Wallet.jsx
@@ -1,9 +1,51 @@
 import React from 'react'
+import solana_icon from '../assets/solana.webp'
+import qnt_icon from '../assets/qnt.webp'
+import render from '../assets/render.webp'
 
 const Wallet = () => {
-  return (
-    <div className=' w-full bg-white py-16 px-4 h-[80vh]'>
 
+  const CryptoCard = ({ bgGradient, image, title, description, imgSize }) => {
+    return (
+      <div className={`lg:w-1/3 ${bgGradient} w-full flex justify-center text-center rounded-3xl transition-transform duration-300 hover:translate-y-3`}>
+        <div className='flex items-center flex-col cursor-pointer'>
+          <img src={image} alt="" className={` w-[${imgSize}] h-auto mt-4 `}/>
+          <h3 className='font-poppins font-semibold text-4xl text-white mt-[15px]'>{title}</h3>
+          <p className='text-center mx-10 text-white mt-4 mb-10 font-poppins font-medium'>{description}</p>
+        </div>
+      </div>
+    );
+  };
+
+  return (
+    <div className=' w-full bg-[#191919] py-16 px-4 h-[80vh]'>
+      <div className='max-w-[1080px] mx-auto flex md:flex-row flex-col gap-11'>
+
+        <CryptoCard 
+          bgGradient=" bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500"
+          image={render}
+          imgSize="245px"
+          title="RNDR"
+          description="The Render Network® Provides Near Unlimited Decentralized GPU Computing Power For Next Generation 3D Content Creation."
+        />
+        
+        <CryptoCard 
+          bgGradient=" bg-gradient-to-r from-indigo-400 to-cyan-500"
+          image={solana_icon}
+          imgSize="250px"
+          title="SOL"
+          description="Get big, quick. Solana is made to handle thousands of transactions per second, and fees for both developers and users remain less than $0.0025."
+        />
+
+        <CryptoCard 
+          bgGradient=" bg-gradient-to-r from-green-400 to-blue-500"
+          image={qnt_icon}
+          imgSize="250px"
+          title="QNT"
+          description="Assets of all kinds, from currencies to carbon credits, are being tokenised on blockchain. Unlocking benefits for all."
+        />
+
+      </div>
     </div>
   )
 }
