25fe175e350d9cf7d89e6e705d0de199c1bf0167
TryanJaya <tryanjaya33.tj@gmail.com>
2024-04-29 11:50:06 +0700

    Crypto Tokens

--- src/components/Wallet.jsx
+++ src/components/Wallet.jsx
@@ -7,24 +7,26 @@ const Wallet = () => {
 
   const CryptoCard = ({ bgGradient, image, title, description, imgSize }) => {
     return (
-      <div className={`lg:w-1/3 ${bgGradient} w-full flex justify-center text-center rounded-3xl transition-transform duration-300 hover:translate-y-3`}>
+      <div className={`lg:w-1/3 ${bgGradient} w-full flex justify-center items-center text-center rounded-3xl transition-transform duration-300 hover:translate-y-3`}>
         <div className='flex items-center flex-col cursor-pointer'>
           <img src={image} alt="" className={` w-[${imgSize}] h-auto mt-4 `}/>
           <h3 className='font-poppins font-semibold text-4xl text-white mt-[15px]'>{title}</h3>
-          <p className='text-center mx-10 text-white mt-4 mb-10 font-poppins font-medium'>{description}</p>
+          <p className='text-center mx-10 text-white md:mt-4 md:mb-10 mt-2 mb-5 font-poppins font-medium'>{description}</p>
         </div>
       </div>
     );
   };
 
   return (
-    <div className=' w-full bg-[#191919] py-16 px-4 h-[80vh]'>
-      <div className='max-w-[1080px] mx-auto flex md:flex-row flex-col gap-11'>
+    <div className=' w-full bg-[#191919] py-16 md:px-6 px-12 md:h-[100vh]'>
+      <h2 className='  text-center text-white font-bold font-poppins mb-10 mt-[-20px] text-4xl'>POPULAR TOKENS</h2>
+      <div className='max-w-[1080px] mx-auto flex md:flex-row flex-col gap-16'>
+
 
         <CryptoCard 
           bgGradient=" bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500"
           image={render}
-          imgSize="245px"
+          imgSize="240px"
           title="RNDR"
           description="The Render Network® Provides Near Unlimited Decentralized GPU Computing Power For Next Generation 3D Content Creation."
         />
@@ -40,7 +42,7 @@ const Wallet = () => {
         <CryptoCard 
           bgGradient=" bg-gradient-to-r from-green-400 to-blue-500"
           image={qnt_icon}
-          imgSize="250px"
+          imgSize="240px"
           title="QNT"
           description="Assets of all kinds, from currencies to carbon credits, are being tokenised on blockchain. Unlocking benefits for all."
         />
