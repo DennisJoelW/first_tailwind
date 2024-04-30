f621fedba05f4cfebf5586ca4be8770500a0b3d2
TryanJaya <tryanjaya33.tj@gmail.com>
2024-04-30 15:41:37 +0700

    Changed name to Nova

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
 
   return (
     <div className='text-white flex justify-between items-center h-24 max-w-7xl mx-auto px-4 pt-4'>
-        <h1 className='w-full lg:text-5xl md:text-5xl font-semibold text-[#03C988] font-poppins text-2xl cursor-pointer'>TELWIN</h1>
+        <h1 className='w-full lg:text-5xl md:text-5xl font-semibold text-[#03C988] font-poppins text-2xl cursor-pointer'>Nova</h1>
         <ul className='hidden md:flex font-poppins '>
           <li className=' p-4  cursor-pointer whitespace-nowrap hover:text-[#03C988] ease-in-out duration-300'>Buy Crypto</li>
           <li className=' p-4 cursor-pointer whitespace-nowrap hover:text-[#03C988] ease-in-out duration-300'>Market</li>
@@ -28,7 +28,7 @@ const Navbar = () => {
         </div>
 
         <div className={menu ? "fixed left-0 top-0 w-[60%] border-r border-r-gray-700 border-b border-b-gray-700 h-full bg-zinc-950 ease-linear duration-300 block md:hidden" : " fixed left-[-100%]"}>
-        <h1 className='w-full text-4xl font-semibold text-[#03C988] m-4 mt-10 font-poppins'>TELWIN</h1>
+        <h1 className='w-full text-4xl font-semibold text-[#03C988] m-4 mt-10 font-poppins'>Nova</h1>
           <ul className='mt-10 font-poppins'>
             <li className=' p-4 font-[16px] cursor-pointer whitespace-nowrap hover:text-[#03C988] ease-in-out duration-300'>Buy Crypto</li>
             <li className=' p-4 font-[16px] cursor-pointer whitespace-nowrap hover:text-[#03C988] ease-in-out duration-300'>Market</li>
